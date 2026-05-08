8238c01ad744c66b7d937da74b87632d505a76b5
AXEL HL <axelherreralira@gmail.com>
2026-05-08 12:44:25 -0600

    Changing outputs

--- ProyectoHolaMundo.java
+++ ProyectoHolaMundo.java
@@ -1,5 +1,7 @@
 public class ProyectoHolaMundo {
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
+        System.out.println("Bienvenidos a java");
+        System.out.println("Y a Git/Github");
     }
 }
