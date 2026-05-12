4fad86cd18df34780eb34ae0dbeceab90dbff415
AXEL HL <axelherreralira@gmail.com>
2026-05-12 13:35:51 -0600

    Persona

--- Persona.java
+++ Persona.java
@@ -2,10 +2,14 @@ public class Persona {
     private String nombre;
     private int edad;
     private String correo;
+    private String salario;
 
-    public Persona(String nombre, int edad, String correo){
+    public Persona(String nombre, int edad, String correo, String salario){
         this.nombre = nombre;
         this.edad = edad;
         this.correo = correo;
+        this.salario = salario;
+
+
     }
 }
